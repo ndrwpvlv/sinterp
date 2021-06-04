b78252990bf6f0b84c044978280c2f6a88091023
ndrwpvlv <ndrw.pvlv@gmail.com>
2021-06-04 15:00:25 +0300

    Tests update

--- tests/test_sinterp.py
+++ tests/test_sinterp.py
@@ -50,3 +50,27 @@ def test_interp1d_6():
         y2 = x2 = 1.0
         x3 = 2.0
         interp1d(x, [x1, x2, x3], [y1, y2])
+
+
+def test_interp1d_7():
+    x = 0.1
+    xp = [0.1, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0]
+    yp = [2.0 * _ for _ in xp]
+    value = interp1d(x, xp, yp)
+    assert value == x * 2.0
+
+
+def test_interp1d_8():
+    x = 2.0
+    xp = [0.1, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0]
+    yp = [2.0 * _ for _ in xp]
+    value = interp1d(x, xp, yp)
+    assert value == x * 2.0
+
+
+def test_interp1d_9():
+    x = 0.25
+    xp = [0.1, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0]
+    yp = [2.0 * _ for _ in xp]
+    value = interp1d(x, xp, yp)
+    assert value == x * 2.0
